b057664d1ff9ab3a11a6c115486a08c420fb6cb6
stela2502 <Stefan.Lang@med.lu.se>
2022-09-14 08:45:44 +0200

    Run now can be called from anywhere.

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-version="1.4"
-singularity run -B/mnt:/mnt SingleCells_v${version}.sif
+version="1.5"
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+singularity run -B/mnt:/mnt ${SCRIPTPATH}/SingleCells_v${version}.sif
