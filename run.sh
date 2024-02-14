62614aab55ce27655078f52be56c260ddcc8fa7d
Stefan Lang <Stefan.Lang@med.lu.se>
2024-02-14 13:24:02 +0100

    My current version of that

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 version="1.5"
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-singularity run -B/mnt:/mnt ${SCRIPTPATH}/SingleCells_v${version}.sif
+echo "singularity run  -B/mnt:/mnt ${SCRIPTPATH}/SingleCells_v${version}.sif"
+singularity run  -B/mnt:/mnt ${SCRIPTPATH}/SingleCells_v${version}.sif
